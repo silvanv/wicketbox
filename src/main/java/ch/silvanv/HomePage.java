ab60ba99b989509b214ff42383876c5d652e3aa7
silvanv <vogelsanger@hotmail.com>
2012-10-23 14:11:37 +0200

    Jetty hot deploy

--- src/main/java/ch/silvanv/HomePage.java
+++ src/main/java/ch/silvanv/HomePage.java
@@ -1,5 +1,8 @@
 package ch.silvanv;
 
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.model.Model;
+
 import ch.silvanv.common.BasePage;
 
 public class HomePage extends BasePage {
@@ -7,5 +10,7 @@ public class HomePage extends BasePage {
     private static final long serialVersionUID = 1L;
 
     public HomePage() {
+    	add(new Label("helloLabel1", Model.of("Hello World")));
+    	add(new Label("helloLabel2", Model.of("Stranger")));
     }
 }
